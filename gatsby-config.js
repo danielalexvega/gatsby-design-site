9ffa5a0dfe7fe291bdfef67a773600bea222880a
Daniel Vega <danielalexvega@gmail.com>
2020-12-21 15:05:55 -0600

    work on Slider component/ set up airtable for reviews

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,11 @@ module.exports = {
             baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
             tableName: `Projects`,
             mapping: { image: `fileNode` }
+          },
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: `Customers`,
+            mapping: { image: `fileNode` }
           }
         ]
       }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Title from "./Title"
 import styled from "styled-components"
@@ -6,9 +6,78 @@ import Image from "gatsby-image"
 import { FaQuoteRight } from "react-icons/fa"
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi"
 
+const query = graphql`
+  {
+    allAirtable(filter: {table: {eq: "Customers"}}) {
+      nodes {
+        data {
+          name
+          quote
+          title
+          image {
+            localFiles {
+              childImageSharp {
+                fixed(width:150, height:150) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const Slider = () => {
-  return <h2>slider component</h2>
+  const { allAirtable: { nodes: customers } } = useStaticQuery(query);
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const lastIndex = customers.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, customers])
+
+  return (
+    <Wrapper className="section">
+      <Title title="Reviews" />
+      <div className="section-center">
+        {customers.map((customer, customerIndex) => {
+          const { data: { image, name, title, quote } } = customer;
+          const customerImg = image.localFiles[0].childImageSharp.fixed;
+
+          let position = 'nextSlide';
+          if (customerIndex === index) {
+            position = 'activeSlide';
+          }
+          if (customerIndex === index - 1 ||
+            (index === 0 && customerIndex === customers.length - 1)) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article className={position} key={customerIndex}>
+              <Image fixed={customerImg} className="img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">
+                {quote}
+              </p>
+              <FaQuoteRight className='icon' />
+            </article>
+          )
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}><FiChevronLeft /> </button>
+        <button className="next" onClick={() => setIndex(index + 1)}><FiChevronRight /> </button>
+      </div>
+    </Wrapper>
+  )
 }
+
 
 const Wrapper = styled.div`
   background: var(--clr-grey-10);
